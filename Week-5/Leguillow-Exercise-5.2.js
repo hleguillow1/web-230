ac7ecdfad3519ed14487c36f5d7d8976cf438c8f
Hector Leguillow <hfleguillow@me.bellevue.edu>
2018-02-11 20:05:13 -0600

    Adding commit

---
+++ Week-5/Leguillow-Exercise-5.2.js
@@ -0,0 +1,32 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ Hector Leguillow
+ Exercise 5.2
+ Today's Date
+
+ Oysters
+ Shrimp
+ Steak
+ Tacos
+ Sushi
+
+ */
+
+// start program 
+var food = ["Oysters", "Shrimp", "Steak", "Tacos", "Sushi"];
+food.forEach(function(food){
+    console.log(food);
+})
+
+
+
+
+
+
+
+
+
+// end program
