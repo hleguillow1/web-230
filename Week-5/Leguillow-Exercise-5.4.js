f366af4b685849616e1baa63617ac81d95ebd18b
Hector Leguillow <hfleguillow@me.bellevue.edu>
2018-02-11 21:14:02 -0600

    Adding commit

---
+++ Week-5/Leguillow-Exercise-5.4.js
@@ -0,0 +1,30 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 5.3
+ Today's Date
+
+ -- COMPOSERS --
+ Last Name: Beethoven, Genre: Classical, Rating: 8
+ Last Name: Mozart, Genre: Classical, Rating: 10
+ Last Name: Bach, Genre: Classical, Rating: 9
+ Last Name: Haydn, Genre: Classical, Rating: 6
+ Last Name: Schubert, Genre: Classical, Rating: 5
+
+ */
+
+// start program
+var composers = ["Beethoven", "Mozart", "Bach", "Hayden", "Schubert"];
+composers.forEach(function(Genre, Rating)
+{
+    console.log(Genre, Rating);
+    
+})
+
+
+
+
+// end program
