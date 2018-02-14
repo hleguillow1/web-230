9e76e7d38d2f9fe914f0347cc54254123a014bd8
Hector Leguillow <hfleguillow@me.bellevue.edu>
2018-02-14 06:34:31 -0800

    Adding commit

---
+++ Week-6/Leguillow-Excercise-6.3.js
@@ -0,0 +1,24 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Exercise 6.3
+ Today's Date
+
+ {id: <ticket id>, name: <ticket name>, requester: <your name>}
+
+ 
+
+// start program
+
+var ticket = {id:"123", name:"Alabama vs Georgia", requester: "Krasso"
+
+};
+console.log("{id, name, requester}"
+
+
+
+// end program
+*/
---
+++ Week-6/Leguillow-Excercise-6.4.js
@@ -0,0 +1,27 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ Assignment 6.4
+ Today's Date
+
+ Ticket <id> was created on <dateCreated> and assigned to employee <firstName lastName> (<jobTitle>).
+
+ */
+
+// start program
+
+var ticket = { ticket.id: "111", Name: John, dateCreated; 10-1:,Priority: none, Person Id: 22,
+Console.log("{id:%s, Name:%s, dateCreated:%s,Prioroty:%s} "
+ticket.id, ticket.name, dateCreated, Priority )
+    // ticket properties
+
+    person: {
+        // person properties
+    }
+};
+
+
+// end program
