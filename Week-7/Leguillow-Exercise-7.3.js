09efbb0b3167bfa131ce41b829a5aa4513d9dd7b
Hector Leguillow <hfleguillow@me.bellevue.edu>
2018-02-14 17:09:24 -0800

    Adding commit

---
+++ Week-7/Leguillow-Exercise-7.3.js
@@ -0,0 +1,27 @@
+!DOCTYPE html>
+<html lang = "en"></html lang = "en">
+ <html> 
+ <head></head>
+<div id = "list"> </div>
+<script></script>
+<h1> SHOPPING LIST v.1 </h1>
+     <ol> </ol>
+
+
+     // inner html goes here
+     
+    <title>Client-Side JavaScript</title>
+ 
+     <link rel="stylesheet" href="../site.css">
+
+
+ <body>
+ 
+ <script>
+     document.write("<h1>Hello World!</h1>")
+ </script>
+ 
+ 
+ 
+ </body>
+ </html>
---
+++ Week-7/Leguillow-Exercise-7.4.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang = "en">
+<head></head>
+<body> 
+    A) 
+    <ahref = 'javascript: alert(newDate.to LocalTimeString());">
+What time is it
+
+</a>
+B)
+<a href="javascript:alert('bar');">
+
+Click me!
+</a>
+
+C) 
+<button  onclick="confirm("Are you sure?');"> SUbmit</button>
+</body>
+</html>
